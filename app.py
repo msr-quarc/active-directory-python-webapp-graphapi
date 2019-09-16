295bf819f91e6004c8bede35dab44fb65686a042
Dave Wecker <dbwz8@hotmail.com>
2019-09-16 09:13:34 -0700

    Cleaned up a bunch of stuff

--- app.py
+++ app.py
@@ -2,42 +2,51 @@ import adal
 import flask
 import uuid
 import requests
-import config
+from config import *
 from urllib.parse import urlencode
 from flask_bootstrap import Bootstrap
 
-app = flask.Flask(__name__)
-app.debug = True
-app.secret_key = 'development'
-bootstrap = Bootstrap(app)
-userName = "None"
+app             = flask.Flask(__name__)
+app.debug       = True
+app.secret_key  = 'development'
+bootstrap       = Bootstrap(app)
 
-########################################################### Local or remote...
-# BASE_URL = 'http://localhost:5000'
-BASE_URL = 'https://weckerPythonWebApp.azurewebsites.net'
+userName        = "None"
 
-AUTHORITY_URL = config.AUTHORITY_HOST_URL + '/' + config.TENANT
-REDIRECT_URI = f'{BASE_URL}/landingPage'
-TEMPLATE_AUTHZ_URL = (AUTHORITY_URL + '/oauth2/authorize?' +
-                      'response_type=code&client_id={}&redirect_uri={}&' +
-                      'state={}&resource={}')
+def buildAuthZ(state):
+    ''' Get a token to accees resource '''
+    url     = f'{AAD_URL}/oauth2/authorize'
+    url    += '?response_type=code'
+    url    += f'&client_id={AAD_ClientId}'
+    url    += f'&redirect_uri={REDIRECT_URI}' 
+    url    += f'&state={state}'
+    url    += f'&resource={GRA_Resource}'
+    return url
 
-
-@app.route("/signout")
-def signout():
-    logout_params = urlencode({'post_logout_redirect_uri':f'{BASE_URL}/logout'})
-    logout_url = f'{AUTHORITY_URL}/oauth2/logout?{logout_params}'
-    resp = flask.Response(status=307)
-    resp.headers['location'] = logout_url
-    userName = "None"
-    return resp
+def openToken(ctxt,code):
+    ''' Crack an AAD token '''
+    return  ctxt.acquire_token_with_authorization_code(
+        code, 
+        REDIRECT_URI, 
+        GRA_Resource, 
+        AAD_ClientId, 
+        AAD_ClientSecret)
 
 @app.route("/logout")
+def signout():
+    logout_params               = urlencode({'post_logout_redirect_uri':f'{BASE_URL}/loggedout'})
+    logout_url                  = f'{AAD_URL}/oauth2/logout?{logout_params}'
+    resp                        = flask.Response(status=307)
+    resp.headers['location']    = logout_url
+    userName                    = "None"
+    return resp
+
+@app.route("/loggedout")
 def logout():
-    index_url = '{}/'.format(BASE_URL)
-    resp = flask.Response(status=307)
-    resp.headers['location'] = index_url
-    userName = "None"
+    index_url                   = '{}/'.format(BASE_URL)
+    resp                        = flask.Response(status=307)
+    resp.headers['location']    = index_url
+    userName                    = "None"
     return resp
 
 @app.route("/")
@@ -47,46 +56,38 @@ def main():
 
 @app.route("/login")
 def login():
-    auth_state = str(uuid.uuid4())
-    flask.session['state'] = auth_state
-    authorization_url = TEMPLATE_AUTHZ_URL.format(
-        config.CLIENT_ID,
-        REDIRECT_URI,
-        auth_state,
-        config.RESOURCE)
-    resp = flask.Response(status=307)
-    resp.headers['location'] = authorization_url
+    auth_state                  = str(uuid.uuid4())
+    flask.session['state']      = auth_state
+    auth_url                    = buildAuthZ(auth_state)
+    resp                        = flask.Response(status=307)
+    resp.headers['location']    = auth_url
     return resp
 
 
 @app.route("/landingPage")
 def crackToken():
-    code = flask.request.args['code']
-    state = flask.request.args['state']
+    code                        = flask.request.args['code']
+    state                       = flask.request.args['state']
     if state != flask.session['state']:
         raise ValueError("State does not match")
-    auth_context = adal.AuthenticationContext(AUTHORITY_URL)
-    token_response = auth_context.acquire_token_with_authorization_code(code, REDIRECT_URI, config.RESOURCE,
-                                                                        config.CLIENT_ID, config.CLIENT_SECRET)
-    # It is recommended to save this to a database when using a production app.
-    flask.session['access_token'] = token_response['accessToken']
-
+    auth_context                    = adal.AuthenticationContext(AAD_URL)
+    token_response                  = openToken(auth_context,code)
+    flask.session['access_token']   = token_response['accessToken']
     return flask.redirect('/graphcall')
-
 
 @app.route('/graphcall')
 def graphcall():
     if 'access_token' not in flask.session:
-        userName = "None"
+        userName    = "None"
         return flask.redirect(flask.url_for('login'))
-    endpoint = config.RESOURCE + '/' + config.API_VERSION + '/me/'
-    http_headers = {'Authorization': 'Bearer ' + flask.session.get('access_token'),
-                    'User-Agent': 'adal-python-sample',
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'client-request-id': str(uuid.uuid4())}
-    graph_data = requests.get(endpoint, headers=http_headers, stream=False).json()
-    userName = graph_data['userPrincipalName']
+    endpoint        = GRA_Resource + '/' + GRA_ApiVersion + '/me/'
+    http_headers    = {'Authorization': 'Bearer ' + flask.session.get('access_token'),
+                        'User-Agent': 'adal-python-sample',
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'client-request-id': str(uuid.uuid4())}
+    graph_data      = requests.get(endpoint, headers=http_headers, stream=False).json()
+    userName        = graph_data['userPrincipalName']
     return flask.render_template('display_graph_info.html', graph_data=graph_data, userName=userName)
     
 if __name__ == "__main__":
--- config.py
+++ config.py
@@ -1,9 +1,28 @@
 import os
-RESOURCE = "https://graph.microsoft.com"  # Add the resource you want the access token for
-TENANT = "microsoft.onmicrosoft.com"  # Enter tenant name, e.g. contoso.onmicrosoft.com
-AUTHORITY_HOST_URL = "https://login.microsoftonline.com"
-CLIENT_ID = "4bb3b469-4d58-4e5d-a33a-a94b6f14901f"  # copy the Application ID of your app from your Azure portal
-CLIENT_SECRET = os.environ['pythonWebApp']  # copy the value of key you generated when setting up the application
 
-# These settings are for the Microsoft Graph API Call
-API_VERSION = 'v1.0'
+# LOCAL or REMOTE:
+BASE_URL        = 'http://localhost:5000'
+#BASE_URL        = 'https://weckerPythonWebApp.azurewebsites.net'
+REDIRECT_URI    = f'{BASE_URL}/landingPage'
+
+# Azure Active Directory
+AAD_Host            =  "https://login.microsoftonline.com" # Authority Host
+AAD_Domain          = "microsoft.onmicrosoft.com" # Tenant
+AAD_URL             = AAD_Host + '/' + AAD_Domain
+AAD_TenantId        = "common"
+AAD_ClientId        = "4bb3b469-4d58-4e5d-a33a-a94b6f14901f"  # AppId
+AAD_Callback        = "/signin-oidc"
+AAD_SignedOut       = "/signout-callback-oidc"
+AAD_ClientSecret    = os.environ['PWA_AAD_ClientSecret']
+
+
+# Fulfillment
+FUL_ClientId        = AAD_ClientId
+FUL_TenantId        = os.environ['PWA_FUL_TenantId']
+FUL_AppKey          = AAD_ClientSecret
+FUL_BaseURI         = "https://marketplaceapi.microsoft.com/api/saas"
+FUL_ApiVersion      = "2018-09-15" # Mock APIs: "2018-09-15", Real APs: "2018-08-31"
+
+# Azure Graph
+GRA_Resource        = "https://graph.microsoft.com"  # Add the resource you want the access token for
+GRA_ApiVersion      = 'v1.0'

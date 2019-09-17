97ffb6f037a398f290bae807a77a962f8e026487
Dave Wecker <dbwz8@hotmail.com>
2019-09-17 08:01:37 -0700

    .

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 BASE_URL            = 'https://weckerContoso.azurewebsites.net'
 
 # Azure Active Directory
-AAD_Host            =  "https://login.microsoftonline.com" # Authority Host
+AAD_Host            = "https://login.microsoftonline.com" # Authority Host
 AAD_Domain          = "microsoft.onmicrosoft.com" # Tenant
 AAD_URL             = AAD_Host + '/' + AAD_Domain
 AAD_TenantId        = "common"
@@ -13,7 +13,6 @@ AAD_ClientId        = "260eae2a-e644-4985-bf6a-c7af0ea1cd98"  # AppId
 AAD_Callback        = "/signin-oidc"
 AAD_SignedOut       = "/signout-callback-oidc"
 AAD_ClientSecret    = os.environ['PWA_AAD_ClientSecret']
-
 
 # Fulfillment
 FUL_Resource        = "62d94f6c-d599-489b-a797-3e10e42fbe22"
